8362c26757b3696197b0ce531a6a84f6a3879ca7
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2021-01-25 10:04:29 +0600

    URI p-1234-Accepted

--- m-uri-1234/src/Main.java
+++ m-uri-1234/src/Main.java
@@ -6,47 +6,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        for (int l = 0; l < 5; l++) {
-            StringBuilder word = new StringBuilder(readLine(inp));
-            if (!word.toString().isEmpty() && word.length() >= 1 && word.length() <= 50 || word.toString().equals(" ")) {
-                int trigger = 1;
-                String[] arr = word.toString().split(" ");
-
-                for (int i = 0; i < arr.length; i++) {
-                    StringBuilder newWord = new StringBuilder(arr[i]);
-                    for (int t = 0; t < arr[i].length(); t++) {
-                        char c = arr[i].charAt(t);
-                        if (trigger == 1) {
-                            trigger--;
-                            if (c > 90) {
-                                c = (char) (c - 32);
-                            }
-                        } else {
-                            trigger++;
-                            if (c < 91) {
-                                c = (char) (c + 32);
-                            }
-                        }
-                        newWord.setCharAt(t, c);
+        while (inp.hasNext()) {
+            String word = inp.nextLine();
+            String answer = "";
+            int cnt = 0;
+            char[] arr = word.toCharArray();
+            for (int i = 0; i < word.length(); i++) {
+                if (Character.isLetter(arr[i])) {
+                    if (cnt % 2 == 0) {
+                        arr[i] = Character.toUpperCase(arr[i]);
+                        answer += arr[i];
+                        cnt++;
+                    } else {
+                        arr[i] = Character.toLowerCase(arr[i]);
+                        answer += arr[i];
+                        cnt++;
                     }
-                    arr[i] = newWord.toString();
-                }
-
-                for (int i = 0; i < arr.length; i++) {
-                    String space = " ";
-                    if (i == arr.length - 1) {
-                        space = "\n";
-                    }
-                    System.out.print(arr[i] + space);
+                }else{
+                    answer += arr[i];
                 }
             }
+            System.out.println(answer);
         }
-    }
-
-    public static String readLine(Scanner inp) {
-        String line = inp.nextLine();
-        while (line.isEmpty())
-            line = inp.nextLine();
-        return line;
     }
 }
