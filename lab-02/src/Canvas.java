a899c0bc95d8f0dbb3aad8d5d71dbbe0739a22f8
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2021-02-17 13:42:38 +0600

    Lab-02 Problem01 Canvas

---
+++ lab-02/src/Canvas.java
@@ -0,0 +1,39 @@
+public class Canvas {
+    public static final int SIZE = 40;
+    public static int counter = 0;
+
+    private char[][] data;
+
+    //    constructor
+    public Canvas() {
+        data = new char[SIZE][SIZE];
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                data[row][col] = '.';
+            }
+        }
+        counter++;
+    }
+
+    //regular method
+    public void set(int row, int col) {
+        if (row < 0 || SIZE <= row || col < 0 || SIZE <= col) {
+            throw new RuntimeException("Improper row or column assigned: [" + row + "," + col + "]");
+        }
+        data[row][col] = '*';
+    }
+
+    public void show() {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(data[row][col]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
