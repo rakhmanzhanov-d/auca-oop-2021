2a0bda65a09610ba53180c11872a0756911878dc
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2021-02-24 14:02:23 +0600

    lab-01 class Problem01

--- lab-03/src/Problem01.java
+++ lab-03/src/Problem01.java
@@ -1,10 +1,76 @@
 import java.util.Scanner;
 
 public class Problem01 {
-    public static void main(String[] args) {
-        Rational a = new Rational(1, 2);
-        Rational b = new Rational(1, 10);
+    static Scanner inp = new Scanner(System.in);
 
-        System.out.printf("%s + %s = %s%n", a, b, a.add(b));
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                readAndCompute();
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static void readAndCompute() {
+        Rational r1 = readRational("Enter 1st rational (EOF for exit): ");
+        String operator = readOperator("Enter operator (EOF for exit): ");
+        Rational r2 = readRational("Enter 2nd rational (EOF for exit): ");
+
+        switch (operator) {
+            case "+" -> System.out.printf("%s + %s = %s%n", r1, r2, r1.add(r2));
+            case "-" -> System.out.printf("%s - %s = %s%n", r1, r2, r1.subtract(r2));
+            case "*" -> System.out.printf("%s * %s = %s%n", r1, r2, r1.multiply(r2));
+            case "/" -> System.out.printf("%s / %s = %s%n", r1, r2, r1.divide(r2));
+            case "=" -> System.out.printf("%s = %s = %s%n", r1, r2, r1.compareTo(r2) == 0);
+            case "!=" -> System.out.printf("%s != %s = %s%n", r1, r2, r1.compareTo(r2) != 0);
+            case "<" -> System.out.printf("%s < %s = %s%n", r1, r2, r1.compareTo(r2) < 0);
+            case ">" -> System.out.printf("%s > %s = %s%n", r1, r2, r1.compareTo(r2) > 0);
+            case "<=" -> System.out.printf("%s <= %s = %s%n", r1, r2, r1.compareTo(r2) <= 0);
+            case ">=" -> System.out.printf("%s >= %s = %s%n", r1, r2, r1.compareTo(r2) >= 0);
+        }
+    }
+
+    private static String readOperator(String msg) {
+        while (true) {
+            System.out.print(msg);
+            if (!inp.hasNextLine()) {
+                System.exit(0);
+            }
+            String res = inp.nextLine().trim();
+            switch (res) {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "=":
+                case "!=":
+                case ">":
+                case ">=":
+                case "<":
+                case "<=":
+                    return res;
+                default:
+                    System.out.println("Incorrect operation: " + res);
+            }
+
+        }
+    }
+
+    private static Rational readRational(String msg) {
+        while (true) {
+            System.out.print(msg);
+            if (!inp.hasNextLine()) {
+                System.exit(0);
+            }
+            String s = inp.nextLine();
+            try {
+                return Rational.parse(s);
+            } catch (RuntimeException e) {
+                System.out.println("Rational num is not correct");
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
