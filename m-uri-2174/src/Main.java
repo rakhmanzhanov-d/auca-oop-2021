7a134bd5fffe9654640d89b8abc33014cf8db11d
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2021-02-28 12:25:17 +0600

    URI p-2174-Accepted

--- m-uri-2174/src/Main.java
+++ m-uri-2174/src/Main.java
@@ -1,8 +1,19 @@
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-//
+        StringBuffer mainString = new StringBuffer();
+        int cnt = 0;
+        Set<String> data = new LinkedHashSet<>();
+        int n = inp.nextInt();
+        for (int i = 0; i <= n; ++i) {
+            String str = inp.nextLine();
+            data.add(str.trim());
+        }
+
+        System.out.println("Falta(m) " + (151 - data.size() + 1) + " pomekon(s).");
     }
 }
