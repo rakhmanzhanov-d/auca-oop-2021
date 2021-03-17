ac97d35adbea5713451a041ba99d9fafcc905e3c
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2021-03-17 09:31:57 +0600

    URI p-1551-Accepted

--- m-uri-1551/src/Main.java
+++ m-uri-1551/src/Main.java
@@ -1,27 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        int test = inp.nextInt();
-        String alpabet = "abcdefghigklmnopqrstuvwxyz";
-
+        int t = inp.nextInt();
         inp.nextLine();
-        for (int t = 0; t < test; t++) {
-            char[] arr = alpabet.toCharArray();
+        for (int i=0; i<t; i++){
+            int res = 0;
+            int[] alphabet = new int[26];
+            Arrays.fill(alphabet, 0);
             String s = inp.nextLine();
-            int cnt = 0;
-            for (int i = 0; i < s.length(); i++) {
-                for (int j = 0; j < alpabet.length(); j++) {
-                    if (arr[j] == s.charAt(i)) {
-                        arr[j] = '*';
-                        cnt++;
-                    }
+            for (int j=0; j<s.length(); j++){
+                if (Character.isLowerCase(s.charAt(j))){
+                    alphabet[s.charAt(j)-'a']++;
                 }
             }
-            if (cnt >= 26) {
+            for (int j=0; j<26; j++){
+                if (alphabet[j] != 0){
+                    res++;
+                }
+            }
+            if (res == 26){
                 System.out.println("frase completa");
-            } else if (cnt >= 13) {
+            } else if (res >= 13){
                 System.out.println("frase quase completa");
             } else {
                 System.out.println("frase mal elaborada");
