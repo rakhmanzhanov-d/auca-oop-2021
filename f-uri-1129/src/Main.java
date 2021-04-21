e33f778581b5122d6c7fd4d10620deb4febd1ac7
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2021-04-21 12:27:46 +0600

    URI p-1129-Accepted

--- f-uri-1129/src/Main.java
+++ f-uri-1129/src/Main.java
@@ -3,55 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        int tests, a, b, c, d, e;
+        int tests;
         while ((tests = inp.nextInt()) != 0) {
             for (int i = 0; i < tests; i++) {
-                int cnt = 0, min = 255;
-                String res = "";
-                a = inp.nextInt();
-                b = inp.nextInt();
-                c = inp.nextInt();
-                d = inp.nextInt();
-                e = inp.nextInt();
-                if (a <= 127) {
-                    cnt++;
-                    if (min >= a) {
-                        min = a;
-                        res = "A";
+                int cnt = 0, position = 0, num;
+                for (int j = 0; j < 5; j++) {
+                    num = inp.nextInt();
+                    if (num <= 127) {
+                        cnt++;
+                        position = j;
                     }
                 }
-                if (b <= 127) {
-                    cnt++;
-                    if (min >= b) {
-                        min = b;
-                        res = "B";
-                    }
-                }
-                if (c <= 127) {
-                    cnt++;
-                    if (min >= c) {
-                        min = c;
-                        res = "C";
-                    }
-                }
-                if (d <= 127) {
-                    cnt++;
-                    if (min >= d) {
-                        min = d;
-                        res = "D";
-                    }
-                }
-                if (e <= 127) {
-                    cnt++;
-                    if (min >= e) {
-                        min = e;
-                        res = "E";
-                    }
-                }
-                if (cnt > 1) {
+                if (cnt == 0 || cnt > 1) {
                     System.out.println("*");
-                } else {
-                    System.out.println(res);
+                } else if (cnt == 1) {
+                    System.out.println((char) ('A' + position));
                 }
             }
         }
