53543f6f8a46b0607a1a162837c56a7b670d95ce
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2021-04-28 12:05:31 +0600

    lab-06 prototype-03 solved

---
+++ lab-06/src/p03/Figure.java
@@ -0,0 +1,16 @@
+package p03;
+
+import java.util.ArrayList;
+
+public abstract class Figure {
+    protected int x;
+    protected int y;
+
+
+    public Figure(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract boolean contains(int x, int y);
+}
